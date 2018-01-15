3eddf48b58057b33a807027c39456476d1c887f2
unknown <151610184@smkassalaambandung.sch.id>
2018-01-15 09:24:35 +0300

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+
+@extends('layouts.1')
+@section('konten')
+<!DOCTYPE html>
+<html>
+<head>
+	<title>tugas</title>
+</head>
+<body>
+<div class="jumbotron">
+        <h1>assalamualaikum</h1>
+        <p>"Jangan berputus asa".</p>
+      </div>
+</body>
+</html>
+@endsection
---
+++ resources/views/profil.blade.php
@@ -0,0 +1,16 @@
+
+@extends('layouts.1')
+@section('konten')
+<!DOCTYPE html>
+<html>
+<head>
+	<title>tugas</title>
+</head>
+<body bgcolor="gold">
+<h1>Nama : Uwoh ayu</h1>
+<h1>Umur : 16 tahun</h1>
+<h1>Kelas : XI RPL 1</h1>
+<h1>Jurusan : RPL</h1>
+</body>
+</html>
+@endsection
